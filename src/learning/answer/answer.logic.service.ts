68899e4bebc88dfe054afc679d8704413a17bd74
krowbee <skoresplay@gmail.com>
2025-11-25 02:52:22 +0200

    feat:add checkAnswer function

--- src/learning/answer/answer.logic.service.ts
+++ src/learning/answer/answer.logic.service.ts
@@ -1,7 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { AnswerService } from './answer.service';
 
 @Injectable()
 export class AnswerLogicService {
   constructor(private answerService: AnswerService) {}
+
+  async checkAnswer(answerId: number): Promise<{ isCorrect: boolean }> {
+    const answer = await this.answerService.getAnswer({ id: answerId });
+    if (!answer) throw new BadRequestException('Incorrect answer');
+    return { isCorrect: answer.isCorrect };
+  }
 }
