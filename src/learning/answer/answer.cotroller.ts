5825e98c25f77931c2f6d7338f993157637a166d
krowbee <skoresplay@gmail.com>
2025-11-25 03:08:36 +0200

    feat:add checkAnswer endpoint to AnswerController

--- src/learning/answer/answer.cotroller.ts
+++ src/learning/answer/answer.cotroller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Post } from '@nestjs/common';
 import { AnswerLogicService } from './answer.logic.service';
 
 @Controller('answer')
 export class AnswerController {
   constructor(private answerLogicService: AnswerLogicService) {}
+
+  @Post('/:taskId/:answerId')
+  async checkAnswer(
+    @Param('answerId') answerId: number,
+    @Param('taskId') taskId: number,
+  ): Promise<{ isCorrect: boolean }> {
+    return this.answerLogicService.checkAnswer(answerId, taskId);
+  }
 }
