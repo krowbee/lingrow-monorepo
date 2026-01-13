ae05695957e4c9d00355752978771f2f63daac69
krowbee <skoresplay@gmail.com>
2026-01-13 22:14:22 +0200

    fix: moved userprogress in education domain

--- src/domains/education/education.module.ts
+++ src/domains/education/education.module.ts
@@ -3,7 +3,7 @@ import { CourseModule } from './course/course.module';
 import { LessonModule } from './lesson/lesson.module';
 import { AnswerModule } from './answer/answer.module';
 import { TaskModule } from './task/task.module';
-import { UserProgressModule } from 'src/userprogress/userprogress.module';
+import { UserProgressModule } from 'src/domains/education/userprogress/userprogress.module';
 
 @Module({
   imports: [
