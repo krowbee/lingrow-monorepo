9a86c9deb9f98ff000803732f5c65a2a13fd8ce5
krowbee <skoresplay@gmail.com>
2026-01-22 19:06:07 +0200

    feat:add logic to logout page

--- app/(features)/auth/[form]/components/logout-form.tsx
+++ app/(features)/auth/[form]/components/logout-form.tsx
@@ -1,12 +1,28 @@
 "use client";
+import Link from "next/link";
 import { Button } from "../../../../../components/ui/button";
 import {
   Card,
   CardDescription,
   CardTitle,
 } from "../../../../../components/ui/card";
+import { logoutOnServer } from "@/lib/api/requests/auth.requests";
+import { useAuthStore } from "@/store/AuthStore";
+import { useRouter } from "next/navigation";
 
 export function LogoutForm() {
+  const logout = useAuthStore((state) => state.logout);
+  const router = useRouter();
+  const onSubmit = async () => {
+    const result = await logoutOnServer();
+    if (result.ok) {
+      logout();
+      router.push("/");
+      return;
+    }
+    return;
+  };
+
   return (
     <div className="flex items-center w-full h-full justify-center">
       <Card className="rounded-xl max-w-[390px] h-min border-none bg-neutral-800 justify-center px-6">
@@ -16,9 +32,17 @@ export function LogoutForm() {
         <CardDescription>
           Ви впевнені що хочете вийти з цього облікового запису?
         </CardDescription>
+
         <div className="flex w-full justify-center gap-4">
-          <Button className="bg-neutral-700 cursor-pointer">Так</Button>
-          <Button className="cursor-pointer">Ні</Button>
+          <Button
+            className="bg-neutral-700 cursor-pointer"
+            onClick={() => onSubmit()}
+          >
+            Так
+          </Button>
+          <Button className="cursor-pointer">
+            <Link href="/courses">Ні</Link>
+          </Button>
         </div>
       </Card>
     </div>
