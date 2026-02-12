c16dfad0e20567e80675ddfb285a192625b56b1c
krowbee <skoresplay@gmail.com>
2026-02-12 23:05:39 +0200

    feat: add url for admin's page header logo

--- app/(admin)/admin/components/AdminHeader.tsx
+++ app/(admin)/admin/components/AdminHeader.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { useAuthStore } from "@/store/AuthStore";
+import { ADMIN_URL } from "@/urls/admin";
+import Link from "next/link";
 import { ReactNode } from "react";
 
 export function AdminHeader(): ReactNode {
@@ -7,7 +9,9 @@ export function AdminHeader(): ReactNode {
   return (
     <header className="w-full min-h flex justify-center border-b p-2">
       <div className="w-full flex flex-col justify-center items-center">
-        <h1 className="font-heading text-2xl">Панель керування</h1>
+        <h1 className="font-heading text-2xl">
+          <Link href={ADMIN_URL.adminPage}>Панель керування</Link>
+        </h1>
         <p className="font-accent">
           <b>{user?.name}</b>
           <b className="text-red-500 uppercase">[{user?.role}]</b>
