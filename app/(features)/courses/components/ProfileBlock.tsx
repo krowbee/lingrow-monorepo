74940a2fad1bd93bfd4b9244367bf6354d8febb9
krowbee <skoresplay@gmail.com>
2026-02-12 17:49:05 +0200

    feat: add check role in courses sidebar

--- app/(features)/courses/components/ProfileBlock.tsx
+++ app/(features)/courses/components/ProfileBlock.tsx
@@ -8,7 +8,14 @@ export function ProfileBlock() {
     <div className="w-full text-md font-bold flex flex-col py-2 px-4 text-center">
       <h3>{user?.name}</h3>
       <p>
-        Підписка: <b className="text-orange-300">FREE</b>
+        Підписка:{" "}
+        {
+          <b
+            className={`${user?.role !== "admin" ? "text-orange-300" : "text-red-500"} uppercase`}
+          >
+            {user?.role === "admin" ? "ADMIN" : "FREE"}
+          </b>
+        }
       </p>
     </div>
   );
