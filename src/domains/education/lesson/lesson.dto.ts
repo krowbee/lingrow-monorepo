6690d2d6955632f86cbadd1a6a5efe39ad27fd1f
krowbee <skoresplay@gmail.com>
2026-02-15 12:49:49 +0200

    feat: add create and update lesson dto

--- src/domains/education/lesson/lesson.dto.ts
+++ src/domains/education/lesson/lesson.dto.ts
@@ -1,14 +1,18 @@
 import { Expose, Type } from 'class-transformer';
 import { TaskDto } from '../task/task.dto';
+
 import {
   IsArray,
   IsEnum,
   IsJSON,
+  IsNotEmpty,
   IsNumber,
+  IsObject,
   IsOptional,
   IsString,
   ValidateNested,
 } from 'class-validator';
+import { OmitType, PartialType } from '@nestjs/swagger';
 
 export enum EnglishLevels {
   A1 = 'A1',
@@ -71,3 +75,33 @@ export class LessonDto {
   @IsNumber()
   order: number;
 }
+
+export class CreateLessonDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsObject()
+  @IsOptional()
+  theory: any;
+
+  @IsString()
+  @IsNotEmpty()
+  slug: string;
+
+  @IsEnum(EnglishLevels)
+  @IsNotEmpty()
+  englishLevel: EnglishLevels;
+
+  @IsNumber()
+  @IsNotEmpty()
+  courseId: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  order: number;
+}
+
+export class UpdateLessonDto extends PartialType(
+  OmitType(CreateLessonDto, ['courseId']),
+) {}
