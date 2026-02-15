e8b4582b57073118451b53f3874a62ce7a9f021e
krowbee <skoresplay@gmail.com>
2026-02-15 13:26:56 +0200

    feat: add create, update, delete endpoints tin lesson controller

--- src/domains/education/lesson/lesson.controller.ts
+++ src/domains/education/lesson/lesson.controller.ts
@@ -1,9 +1,28 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { LessonService } from './lesson.service';
-import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CurrentUser } from 'src/domains/auth/decorators/current-user.decorator';
 import type { TokenPayload } from 'src/domains/auth/types/authTypes';
-import { AuthOnly } from 'src/domains/auth/decorators/auth.decorators';
+import {
+  AdminOnly,
+  AuthOnly,
+} from 'src/domains/auth/decorators/auth.decorators';
+import { CreateLessonDto, LessonDto, UpdateLessonDto } from './lesson.dto';
 @ApiTags('Lesson API')
 @Controller('lessons')
 export class LessonController {
@@ -25,5 +44,44 @@ export class LessonController {
       user.id,
     );
     return { lesson };
+  }
+
+  @ApiOperation({
+    summary: 'Create lesson (AdminOnly)',
+    description: 'Return lesson object',
+  })
+  @ApiBody({ type: CreateLessonDto })
+  @AdminOnly()
+  @Post('/')
+  async createLesson(@Body() data: CreateLessonDto) {
+    const lesson = await this.lessonService.createLesson(data);
+    return lesson;
+  }
+
+  @ApiOperation({
+    summary: 'Update lesson (AdminOnly)',
+    description: 'Return lesson object',
+  })
+  @ApiBody({ type: UpdateLessonDto })
+  @ApiParam({ name: 'lessonId', type: Number })
+  @Patch('/:lessonId')
+  async updateLesson(
+    @Body() data: UpdateLessonDto,
+    @Param('lessonId', ParseIntPipe) lessonId: number,
+  ) {
+    const lesson = await this.lessonService.updateLesson(data, lessonId);
+    return lesson;
+  }
+
+  @ApiOperation({
+    summary: 'Delete lesson (AdminOnly)',
+    description: 'Return lesson object',
+  })
+  @ApiParam({ name: 'lessonId', type: Number })
+  @ApiOkResponse({ type: LessonDto })
+  @Delete('/:lessonId')
+  async deleteLesson(@Param('lessonId', ParseIntPipe) lessonId: number) {
+    const lesson = await this.lessonService.deleteLesson(lessonId);
+    return lesson;
   }
 }
