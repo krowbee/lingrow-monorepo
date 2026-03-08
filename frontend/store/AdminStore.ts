d6198237ee292e06328abc6de967233ef439e6fe
krowbee <skoresplay@gmail.com>
2026-03-08 16:21:58 +0200

    feat: updated AdminStore

--- frontend/store/AdminStore.ts
+++ frontend/store/AdminStore.ts
@@ -1,19 +1,41 @@
 import { Course, Lesson } from "@/types/course/course";
 import { create } from "zustand";
 
-interface AdminStore {
-  coursesList: Course[];
-  currentCourse: Course | null;
-  choosedCourseLessons: Lesson[];
-  isLoading: boolean;
-  error: string | null;
-
-  // Екшени
-  setCoursesList: (courses: Course[]) => void;
-  chooseCourse: (course: Course) => void;
-
-  // Асинхронні запити
-  fetchCourses: () => Promise<void>;
-  fetchLessons: (slug: string) => Promise<void>;
-  createNewLesson: (lessonData: Partial<Lesson>) => Promise<void>;
+interface OpenMenuPayload {
+  entity: "course" | "lesson";
+  action: "create" | "edit";
+  id?: number;
 }
+
+interface AdminStore {
+  errorMessage: string | null;
+  setErrorMessage: (message: string) => void;
+  openMenu: null | OpenMenuPayload;
+  openForm: (payload: OpenMenuPayload) => void;
+  closeForm: () => void;
+  choosedEditCourse: Course | null;
+  setEditCourse: (course: Course) => void;
+  choosedEditLesson: Lesson | null;
+  setEditLesson: (lesson: Lesson) => void;
+}
+
+export const useAdminStore = create<AdminStore>()((set) => ({
+  choosedEditCourse: null,
+  choosedEditLesson: null,
+  errorMessage: null,
+  openMenu: null,
+  setErrorMessage: (message: string) => {
+    try {
+      set({ errorMessage: message });
+    } finally {
+      setTimeout(() => {
+        set({ errorMessage: null });
+      }, 3000);
+    }
+  },
+  openForm: (payload: OpenMenuPayload) => set({ openMenu: payload }),
+  closeForm: () =>
+    set({ openMenu: null, choosedEditCourse: null, choosedEditLesson: null }),
+  setEditCourse: (course: Course | null) => set({ choosedEditCourse: course }),
+  setEditLesson: (lesson: Lesson | null) => set({ choosedEditLesson: lesson }),
+}));
