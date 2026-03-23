83eeb3d0ada386ef613579875748303ad18b3d59
krowbee <skoresplay@gmail.com>
2026-03-23 21:18:03 +0200

    fix: cookie fix

--- backend/src/domains/auth/constants/cookieOptions.ts
+++ backend/src/domains/auth/constants/cookieOptions.ts
@@ -3,7 +3,8 @@ import { CookieOptions } from 'express';
 export const refreshCookieOptions: CookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
-  sameSite: process.env.NODE_ENV === 'production' ? 'lax' : ('strict' as const),
+  sameSite:
+    process.env.NODE_ENV === 'production' ? 'none' : ('strict' as const),
   maxAge: 7 * 24 * 60 * 60 * 1000,
   path: '/',
 };
@@ -11,7 +12,8 @@ export const refreshCookieOptions: CookieOptions = {
 export const accessCookieOptions: CookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
-  sameSite: process.env.NODE_ENV === 'production' ? 'lax' : ('strict' as const),
+  sameSite:
+    process.env.NODE_ENV === 'production' ? 'none' : ('strict' as const),
   maxAge: 10 * 60 * 1000,
   path: '/',
 };
