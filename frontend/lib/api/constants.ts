4cf059a2c2643ce9ffbb8de78de54ec273593c94
krowbee <skoresplay@gmail.com>
2026-03-23 20:03:03 +0200

    fix: fixed problems

--- frontend/lib/api/constants.ts
+++ frontend/lib/api/constants.ts
@@ -1,4 +1,7 @@
 const isClient = typeof window !== "undefined";
-export const API_URL = isClient
-  ? "http://localhost:3003"
-  : process.env.NEXT_PUBLIC_API_URL;
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? process.env.NEXT_PUBLIC_API_URL
+    : isClient
+      ? "http://localhost:3003"
+      : process.env.NEXT_PUBLIC_API_URL;
