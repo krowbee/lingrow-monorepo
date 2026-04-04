7bbf57ce0cec466f7aea9cfe977521137960c415
krowbee <skoresplay@gmail.com>
2026-04-04 15:15:54 +0300

    feat: add logout button to sidebar

--- frontend/app/(features)/courses/components/ProfileBlock.tsx
+++ frontend/app/(features)/courses/components/ProfileBlock.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useAuthStore } from "@/store/AuthStore";
-import { User } from "lucide-react";
+import { AUTH_URLS } from "@/urls/auth";
+import { LogOut, User } from "lucide-react";
+import Link from "next/link";
 
 export function ProfileBlock() {
   const user = useAuthStore((state) => state.user);
@@ -10,15 +12,25 @@ export function ProfileBlock() {
       <div className="flex rounded-full text-purple-500 border border-purple-500/25 p-3  text-center justify-center items-center text-lg">
         <User />
       </div>
-      <div className="flex flex-col">
-        <h3>{user?.name}</h3>
-        <p>
-          {
-            <b className={`text-purple-400 font-semibold uppercase`}>
-              {user?.role === "admin" ? "ADMIN" : "FREE"}
-            </b>
-          }
-        </p>
+      <div className="flex flex-row items-center justify-between w-full">
+        <div className="flex flex-col">
+          <h3>{user?.name}</h3>
+          <p>
+            {
+              <b className={`text-purple-400 font-semibold uppercase`}>
+                {user?.role === "admin" ? "ADMIN" : "FREE"}
+              </b>
+            }
+          </p>
+        </div>
+        <Link
+          href={AUTH_URLS.logout}
+          className="border border-white/10 group/logout hover:bg-purple-500/10 p-2 rounded-xl cursor-pointer"
+        >
+          <span className="text-white group-hover/logout:text-purple-500 duration-300">
+            <LogOut />
+          </span>
+        </Link>
       </div>
     </div>
   );
