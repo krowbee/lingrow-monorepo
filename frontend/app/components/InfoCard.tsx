5d2e40a30cfbbe8b5bc50d22009cceb2c6665964
krowbee <skoresplay@gmail.com>
2026-04-08 01:05:09 +0300

    feat: polished about section

--- frontend/app/components/InfoCard.tsx
+++ frontend/app/components/InfoCard.tsx
@@ -14,10 +14,12 @@ export const InfoCard = ({
 }): React.ReactNode => {
   return (
     <Card
-      className={`w-[375px] min-h-[260px] max-w-sm bg-neutral-800/10 border rounded-sm border-white/5`}
+      className={`w-[375px] min-h-[260px] cursor-default transition-transform max-w-sm bg-neutral-800/10 border rounded-sm border-white/5 transition duration-600 hover:-translate-y-2 hover:shadow-xl hover:shadow-fuchsia-500 group`}
     >
       <CardHeader className="gap-4">
-        <CardTitle className={`text-white text-xl font-heading`}>
+        <CardTitle
+          className={`text-white text-xl font-heading group-hover:text-fuchsia-400 transition duration-600`}
+        >
           {cardTitle}
         </CardTitle>
         <CardDescription className="text-lg text-neutral-400 max-w-auto font-body leading-[1.8]">
