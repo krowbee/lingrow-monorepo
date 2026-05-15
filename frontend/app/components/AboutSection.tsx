0cb703ccfaa259f7dd839cfb7bbff58daeb2d9a2
krowbee <skoresplay@gmail.com>
2026-05-15 20:23:58 +0300

    feat: add spacing to about us

--- frontend/app/components/AboutSection.tsx
+++ frontend/app/components/AboutSection.tsx
@@ -60,13 +60,13 @@ export const AboutSection = () => {
   return (
     <section
       id="about"
-      className="about relative flex w-full flex-col items-center justify-center gap-10 shadow-md"
+      className="about relative flex w-full flex-col items-center justify-center gap-4 shadow-md"
     >
       <AboutBackground />
-      <h1 className="font-heading z-10 flex w-full items-center justify-center p-8 text-5xl">
+      <h1 className="font-heading z-10 flex w-full items-center justify-center px-8 py-6 text-5xl">
         Про нас
       </h1>
-      <div className="z-10 grid w-full justify-items-center gap-y-20 lg:grid-cols-2 xl:grid-cols-3">
+      <div className="z-10 grid w-full justify-items-center gap-y-20 pb-6 lg:grid-cols-2 xl:grid-cols-3">
         {Object.values(ABOUT_CARD_CONTENT).map((card, index) => (
           <InfoCard
             key={index}
