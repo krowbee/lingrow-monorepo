b32e2a7a3908ac537e5c52acc85fa0b15cb4c9aa
krowbee <skoresplay@gmail.com>
2026-05-16 17:48:39 +0300

    feat: removed admin tables

--- frontend/app/(protected)/(admin)/admin/components/TableContainer.tsx
+++ frontend/app/(protected)/(admin)/admin/components/TableContainer.tsx
@@ -1,15 +1,5 @@
 "use client";
-import { Course } from "@/types/course/course";
-import { useState } from "react";
-import { CoursesTable } from "./CoursesTable";
-import { LessonsTable } from "./LessonsTable";
 
 export function TableContainer() {
-  const [choosedCourse, setChoosedCourse] = useState<Course | null>(null);
-  return (
-    <div className="flex w-full">
-      <CoursesTable setChoosedCourse={setChoosedCourse} />
-      <LessonsTable choosedCourse={choosedCourse} />
-    </div>
-  );
+  return <div className="flex w-full"></div>;
 }
