b19ade9ac636d068653881f735b69762407bd3b1
krowbee <skoresplay@gmail.com>
2026-05-19 02:06:05 +0300

    feat: optimized about us bg loading

--- frontend/app/components/about-us/AboutBackGround.tsx
+++ frontend/app/components/about-us/AboutBackGround.tsx
@@ -3,11 +3,11 @@ export function AboutBackground() {
     <div className="inset-x absolute z-0 h-full w-full overflow-hidden">
       <picture>
         <source
-          src="/about-us/about_us_bg_desktop.webp"
+          srcSet="/about-us/about_us_bg_desktop.webp"
           media="(min-width:1024px)"
           type="image/webp"
         />
-        <source src="/about-us/about_us_bg_mobile.webp" type="image/webp" />
+        <source srcSet="/about-us/about_us_bg_mobile.webp" type="image/webp" />
         <img
           src="/about-us/about_us_bg_mobile.png"
           alt=""
